d665bb715f6cd25cc3f2d9ad356e577a9503cc3f
Hyeonseok An <ajy720@gmail.com>
2025-11-17 11:57:18 +0000

    init Project

---
+++ src/main/java/sbhackathon/koala/happyMSP/HappyMspApplication.java
@@ -0,0 +1,13 @@
+package sbhackathon.koala.happyMSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyMspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sbhackathon/koala/happyMSP/HappyMspApplicationTests.java
@@ -0,0 +1,13 @@
+package sbhackathon.koala.happyMSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyMspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
